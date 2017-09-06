fc50047c5da2a8c9af787119514bf8228eff8933
Ridenhour <mtr1218@live.missouristate.edu>
2017-09-06 13:56:44 -0500

    init commit

---
+++ Interview_challenge.js
@@ -0,0 +1,16 @@
+function MSUBears(){
+	
+	for(i = 1; i <= 100; i++){
+		if(i%3==0 && i%5==0){
+			console.log("MSUBears");
+        }else if(i%5==0){
+			console.log("Bears");
+        }else if(i%3==0){
+			console.log("MSU");
+        }else{
+			console.log(i);
+        }
+    }
+}
+
+MSUBears();
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # MSU_web_challenge
+Welcome to my README file!
+
+The code that was upload was for the MSU web programmer challenge.
